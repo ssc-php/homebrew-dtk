2bdffee748488090676090c8daeea2721eef48c7
Loïc Faugeron <faugeron.loic@gmail.com>
2026-04-11 16:01:33 +0200

    chore: release v0.2.0

--- Formula/dtk.rb
+++ Formula/dtk.rb
@@ -2,29 +2,29 @@ class Dtk < Formula
   desc "Kanban, Git and Deployment, in one coherent flow"
   homepage "https://github.com/ssc-php/dtk"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ssc-php/dtk/releases/download/v#{version}/dtk-macos-x86_64"
-      sha256 "3e44b1c8583b9f5cc140acbc43726de81fb8a151c0afc43e1152729c9a06213b"
+      url "https://github.com/ssc-php/dtk/releases/download/v\#{version}/dtk-macos-x86_64"
+      sha256 "172441a992d7b85d9c60a1de4b251a73b7dd23af4d1ddb9df2851154630aed6f"
     end
 
     on_arm do
-      url "https://github.com/ssc-php/dtk/releases/download/v#{version}/dtk-macos-aarch64"
-      sha256 "957953df74861df6cb8f975c4864f4495277c63cdd8b73fa20f9884dd2538ca0"
+      url "https://github.com/ssc-php/dtk/releases/download/v\#{version}/dtk-macos-aarch64"
+      sha256 "0fb5be242e2815d7fd986d6388c1ec0862dd3486649eeb5d918346a3c5ccf3c7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ssc-php/dtk/releases/download/v#{version}/dtk-linux-x86_64"
-      sha256 "4870846397532db7e513b39cabfd3b6fba426fbb3232190480ffc2368a51da27"
+      url "https://github.com/ssc-php/dtk/releases/download/v\#{version}/dtk-linux-x86_64"
+      sha256 "618971a3a885015bad74101df0422a644a4a7e8a9f52ff2ccd1b813d7c066aac"
     end
 
     on_arm do
-      url "https://github.com/ssc-php/dtk/releases/download/v#{version}/dtk-linux-aarch64"
-      sha256 "e4674f606d96fcbf2fe80505d26a4f260ef27f488b5d0e56e5193f54a9ebd263"
+      url "https://github.com/ssc-php/dtk/releases/download/v\#{version}/dtk-linux-aarch64"
+      sha256 "9d7529e8ab8a4543412be5540d2b5047ffe1951da243e54618872ab9dc898cc4"
     end
   end
 
@@ -33,6 +33,6 @@ class Dtk < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/dtk --version")
+    assert_match version.to_s, shell_output("\#{bin}/dtk --version")
   end
 end
